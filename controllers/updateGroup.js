604ead24433f6f2799cdd1d13642aa3c6a1068ab
MantisA-Z <160739281+MantisA-Z@users.noreply.github.com>
2024-11-14 22:43:51 +0530

    Added code to update group on the basis of the edit object sent by user and also removing the users from the group which were in the remove array of the edit object, and at last respond with the members and the updated group

--- controllers/updateGroup.js
+++ controllers/updateGroup.js
@@ -1,5 +1,6 @@
 const groupModel = require("../models/groups");
 const userModel = require("../models/userModel");
+const { uploadToCloudinary } = require("../utils/cloudinary");
 
 const updateGroup = async (req, res) => {
   const { groupId, edit } = req.body;
@@ -9,7 +10,7 @@ const updateGroup = async (req, res) => {
     return;
   }
   try {
-    let group = await groupModel.findOne({ _id: groupId });
+    const group = await groupModel.findOne({ _id: groupId });
     if (!group) {
       res.status(404).json({ err: "not found" });
       return;
@@ -20,8 +21,7 @@ const updateGroup = async (req, res) => {
       return;
     }
 
-    //If everything is correct and user is in the group then allow him to update group
-    //check if there is an edit field or not
+    //check if there is an edit field or not, if not then just send the group and members info
     if (!edit) {
       const members = await Promise.all(
         group.members.map(async (connectionId) => {
@@ -31,6 +31,45 @@ const updateGroup = async (req, res) => {
       );
       res.status(200).json({ group, members });
       return;
+    }
+    //If there is a edit in the req body then edi the group
+    const e = {
+      name: edit.name || group.name,
+      logoFile: edit.logoFile || group.logoFile,
+      remove: edit.remove || [],
+    };
+    try {
+      const file = await uploadToCloudinary(e.logoFile);
+      const updatedMembers = group.members.filter(
+        (member, i) => !e.remove.includes(member)
+      );
+      group.name = e.name;
+      group.logoFile = file.url;
+      group.members = updatedMembers;
+      const updatedGroup = await group.save();
+
+      //update the removed users group key from the DB
+      await Promise.all(
+        e.remove.map(async (connectionId, i) => {
+          const user = await userModel.findOne({ connectionId });
+          if (!user) return;
+          const userUpdatedGroups = user.groups.filter(
+            (_id, i) => _id.toString() !== group._id.toString()
+          );
+          user.groups = userUpdatedGroups;
+          await user.save();
+        })
+      );
+      const members = await Promise.all(
+        group.members.map(async (connectionId) => {
+          const member = await userModel.findOne({ connectionId });
+          return member;
+        })
+      );
+      res.status(200).json({ group: updatedGroup, members });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ err: "Internal server error" });
     }
   } catch (err) {
     console.log(err);
